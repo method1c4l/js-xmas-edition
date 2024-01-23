b901657773f572e40804f5b23af75d0c56e3b1cb
method1c4l <98427344+method1c4l@users.noreply.github.com>
2024-01-22 22:01:37 -0300

    Agrega la tarea de la clase 8

--- js/main.js
+++ js/main.js
@@ -1,25 +1,114 @@
 const $formulario = document.formulario;
 
-const nombreUsuario = $formulario.nombre.value;
-// $nombreUsuario = document.querySelector('[name=nombre]');
-// $nombreUsuario = document.querySelector('#nombre');
-
-const ciudadUsuario = $formulario.ciudad.value;
-const comportamientoUsuario = $formulario['comportamiento'].value;
-const descripcionRegalo = $formulario['descripcion-regalo'].value;
+$formulario.onsubmit = validarFormulario;
+// Cliente -> $formulario.onsubmit(new ClickEvent());
 
 
-function validarNombre(nombreUsuario) {
-    if (nombreUsuario.length === 0) {
-        return 'Este campo debe tener al menos 1 caracter';
+function validarFormulario(event) {
+    const nombreUsuario = $formulario.nombre.value;
+    // $nombreUsuario = document.querySelector('[name=nombre]');
+    // $nombreUsuario = document.querySelector('#nombre');
+
+    const ciudadUsuario = $formulario.ciudad.value;
+    const descripcionRegalo = $formulario['descripcion-regalo'].value;
+
+
+    const errorNombre = validarNombre(nombreUsuario);
+    const errorCiudad = validarCiudad(ciudadUsuario);
+    const errorDescripcionRegalo = validarDescripcionRegalo(descripcionRegalo);
+
+    const errores = {
+        nombre: errorNombre,
+        ciudad: errorCiudad,
+        'descripcion-regalo': errorDescripcionRegalo
+        // descripcionRegalo: value
     }
 
-    if (nombreUsuario.length >= 50) {
-        return 'Este campo debe tener menos de 50 caracteres';
+    const hayErrores = manejarErrores(errores) > 0;
+
+    if (!hayErrores) {
+        $formulario.className = 'oculto';
+
+        document.querySelector('#exito').className = '';
+
+        setTimeout(function () {
+            window.location.href = 'wishlist.html';
+        }, 5000);
     }
 
-    return '';
+
+    event.preventDefault();
 }
+
+
+
+function manejarErrores(errores) {
+    /* DYNAMIC */
+    const keys = Object.keys(errores);
+
+    let contadorErrores = 0;
+
+    keys.forEach(function (key) {
+        const error = errores[key];
+
+        const elementoError = document.querySelector(`.${key}`);
+        const errorExiste = Boolean(elementoError);
+
+        if (error && !errorExiste) {
+            $formulario[key].className = 'error';
+            // Esto solo funciona si la "key" tiene el mismo nombre que el "name" del elemento en HTML.
+
+            mostrarError(key, error);
+
+            contadorErrores += 1;
+        } else if (!error && errorExiste) {
+            $formulario[key].className = '';
+
+            elementoError.remove();
+        } else if (error && errorExiste) {
+            elementoError.textContent = error;
+
+            contadorErrores += 1;
+        }
+    })
+
+    return contadorErrores;
+
+
+    /* HARD-CODED */
+    // errorNombre = errores.nombre;
+    // errorCiudad = errores.ciudad;
+    // errorDescripcionRegalo = errores['descripcion-regalo'];
+
+
+    // if (errorNombre) {
+    //     $formulario.nombre.className = 'error';
+    // } else {
+    //     $formulario.nombre.className = '';
+    // }
+
+    // if (errorCiudad) {
+    //     $formulario.ciudad.className = 'error';
+    // } else {
+    //     $formulario.ciudad.className = '';
+    // }
+
+    // if (errorDescripcionRegalo) {
+    //     $formulario['descripcion-regalo'].className = 'error';
+    // } else {
+    //     $formulario['descripcion-regalo'].className = '';
+    // }
+}
+
+function mostrarError(key, error) {
+    const $error = document.createElement('li');
+    $error.classList.add(key);
+    $error.textContent = error;
+
+    const $erroresContainer = document.querySelector('#errores');
+    $erroresContainer.appendChild($error);
+}
+
 
 // function validarNombre(nombreUsuario) {
 //     if (nombreUsuario.length === 0) {
@@ -31,9 +120,25 @@ function validarNombre(nombreUsuario) {
 //     }
 // }
 
+function validarNombre(nombreUsuario) {
+    if (nombreUsuario.length === 0) {
+        return 'El campo de nombre debe tener al menos 1 caracter';
+    }
+
+    if (nombreUsuario.length >= 50) {
+        return 'El campo de nombre debe tener menos de 50 caracteres';
+    }
+
+    if (!/^[a-z]+$/i.test(nombreUsuario)) {
+        return 'El campo de nombre sólo acepta letras';
+    }
+
+    return '';
+}
+
 function validarCiudad(ciudadUsuario) {
     if (ciudadUsuario.length === 0) {
-        return 'Este campo es obligatorio';
+        return 'El campo de ciudad es obligatorio';
     }
 
     return '';
@@ -41,12 +146,59 @@ function validarCiudad(ciudadUsuario) {
 
 function validarDescripcionRegalo(descripcionRegalo) {
     if (descripcionRegalo.length === 0) {
-        return 'Este campo debe tener al menos 1 caracter';
+        return 'El campo de descripción debe tener al menos 1 caracter';
     }
 
     if (descripcionRegalo.length >= 100) {
-        return 'Este campo debe tener menos de 100 caracteres';
+        return 'El campo de descripción debe tener menos de 100 caracteres';
+    }
+
+    // [a-z0-9,\._ ] // "." === any character
+    if (!/^[a-z0-9]+$/i.test(descripcionRegalo)) {
+        return 'El campo de descripción sólo acepta letras y números';
     }
 
     return '';
 }
+
+
+
+/* Event Bubbling */
+
+// It happens when an element receives an event, and that event bubbles up
+// (or you can say is transmitted or propagated) to its parent and ancestor elements in the DOM tree
+// until it gets to the root element (html). This is the default beheavior of events on elements
+// unless you stop the propagation (event.preventDefault()).
+
+
+/* JSON - JavaScript Object Notation */
+
+// const objeto = {
+//     p1: 'Hola 1',
+//     p2: function () { console.log('Hola 2'); },
+//     p3: 123,
+//     p4: { p5: 'Hola 5' },
+//     p6: [{ p7: 'Hola 7' }]
+// }
+
+// objeto.p1 === 'Hola 1'
+
+// objeto.p2 === f() { console.log('Hola 2'); }
+// objeto.p2() === Hola 2
+
+// objeto.p3 === 123
+
+// objeto.p4 === { p5: 'Hola 5' }
+// objeto.p4.p5 === 'Hola 5'
+
+// objeto.p6 === [{...}]
+// objeto.p6[0] === { p7: 'Hola 7' }
+// objeto.p6[0].p7 === 'Hola 7'
+
+
+// keys.forEach(function (
+//     Even if empty, the client uses each element as argument. Doesn't matter what name you put in here.
+//     ) {...})
+// element.onclick = function (
+//     Even if empty, the client uses the event as argument. Doesn't matter what name you put in here.
+//     ) {...}
--- js/pruebas.js
+++ js/pruebas.js
@@ -1,12 +1,17 @@
 function testValidarNombre() {
     console.assert(
-        validarNombre('') === 'Este campo debe tener al menos 1 caracter',
+        validarNombre('') === 'El campo de nombre debe tener al menos 1 caracter',
         'validarNombre no pudo validar que el nombre no sea vacío'
     )
 
     console.assert(
-        validarNombre('11111111111111111111111111111111111111111111111111') === 'Este campo debe tener menos de 50 caracteres',
+        validarNombre('llllllllllllllllllllllllllllllllllllllllllllllllll') === 'El campo de nombre debe tener menos de 50 caracteres',
         'validarNombre no pudo validar que el nombre no supere los 50 caracteres'
+    )
+
+    console.assert(
+        validarNombre('12345') === 'El campo de nombre sólo acepta letras',
+        'validarNombre no pudo validar que el nombre sólo tenga letras'
     )
 
     console.assert(
@@ -20,7 +25,7 @@ testValidarNombre();
 
 function testValidarCiudad() {
     console.assert(
-        validarCiudad('') === 'Este campo es obligatorio',
+        validarCiudad('') === 'El campo de ciudad es obligatorio',
         'validarCiudad no pudo validar que la ciudad no sea vacía'
     )
 
@@ -35,15 +40,20 @@ testValidarCiudad();
 
 function testValidarDescripcionRegalo() {
     console.assert(
-        validarDescripcionRegalo('') === 'Este campo debe tener al menos 1 caracter',
+        validarDescripcionRegalo('') === 'El campo de descripción debe tener al menos 1 caracter',
         'validarDescripcionRegalo no pudo validar que la descripción del regalo no sea vacía'
     )
 
     console.assert(
         validarDescripcionRegalo(
-            '1111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111'
-        ) === 'Este campo debe tener menos de 100 caracteres',
+            'llllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllll'
+        ) === 'El campo de descripción debe tener menos de 100 caracteres',
         'validarDescripcionRegalo no pudo validar que la descripción del regalo no supere los 100 caracteres'
+    )
+
+    console.assert(
+        validarDescripcionRegalo('@&%#$') === 'El campo de descripción sólo acepta letras y números',
+        'validarDescripcionRegalo no pudo validar que la descripción del regalo sólo tenga letras y números'
     )
 
     console.assert(
